52eae5e1ca0cb07d7702019476d8123d5eaf6ef5
Jon Zielenkievicz <jonzielen@gmail.com>
2016-03-08 22:23:45 -0500

    style table

--- js/app/controllers/statsController.js
+++ js/app/controllers/statsController.js
@@ -2,11 +2,15 @@ var siteApp = angular.module('statsPageApp', []);
 
 siteApp.controller('StatsCtrl', function($scope, $http) {
     // json call to data
-    $http.get('https://mockaroo.com/e7995d70/download?count=5000&key=015777f0').then(successCallback, errorCallback);
+    // LIVE DATA $http.get('https://mockaroo.com/e7995d70/download?count=5000&key=015777f0').then(successCallback, errorCallback);
+
+    // LOCAL DATA
+    $http.get('js/assignment-table-data.json').then(successCallback, errorCallback);
+
 
     // successful json call
-    function successCallback(data) {
-      $scope.data = data;
+    function successCallback(response) {
+      $scope.data = response.data;
     }
 
 

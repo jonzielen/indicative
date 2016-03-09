52b7cd314cda178fe0760d37960744c41a4e8acc
Jon Zielenkievicz <jonzielen@gmail.com>
2016-03-08 22:54:34 -0500

    update html, add js

--- js/src/main.js
+++ js/src/main.js
@@ -1,3 +1,10 @@
 (function() {
-  console.log('test');
+  'use strict';
+
+  var header = document.getElementById('stats-header'),
+  statsCol = document.getElementById('stats-columns');
+
+  header.addEventListener('click', function() {
+    statsCol.classList.toggle('show');
+  });
 })();

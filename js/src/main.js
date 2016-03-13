f08ad59243323e94a6242ef9efde95c85eec2fcf
Jon Zielenkievicz <jonzielen@gmail.com>
2016-03-13 16:53:32 -0400

    js clean up

--- js/src/main.js
+++ js/src/main.js
@@ -4,28 +4,21 @@
   var header = document.getElementById('stats-header'),
   statsCol = document.getElementById('stats-columns');
 
-  // show/hide columns when header is clicked
+  // show/hide columns when header is clicked, adjust table body height
   header.addEventListener('click', function() {
     statsCol.classList.toggle('show');
     tableAdjust();
   });
 
-
-
+  // adjust table body height
   function tableAdjust() {
     var viewPortHeight = window.innerHeight,
     upperPart = document.querySelector('.upper').clientHeight,
     thead = document.querySelector('table thead').clientHeight,
     tbody = document.querySelector('table tbody');
-    // need table top margin and th heught
-
-    console.log(upperPart);
-
 
     tbody.style.height = viewPortHeight - upperPart - thead + 'px';
   }
 
   tableAdjust();
-
-
 })();

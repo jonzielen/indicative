b874e64345907cf7a74f12b42e924b588a69060b
Jon Zielenkievicz <jonzielen@gmail.com>
2016-03-12 21:12:08 -0500

    clean up

--- js/app/controllers/statsController.src.js
+++ js/app/controllers/statsController.src.js
@@ -12,20 +12,26 @@ siteApp.controller('StatsCtrl', function($scope, statsFactory) {
 
   // successful json call
   function successCallback(response) {
-    console.log('called');
     $scope.users = response;
-    $scope.totalRecords = response.length;
 
-    // counts
-    var count = {
-      males: [],
-      females: [],
-      fullColorList: [],
-      signedup: [],
-      viewedProfile: [],
-      viewedItem: [],
-      purchasedItem: [],
-      conversions: []
+    // stats
+    var stats = {
+      percents: {
+        males: [],
+        females: [],
+        conversions: [],
+        conversionsViewedProfile: [],
+        conversionsViewedItem: []
+      },
+      counts: {
+        totalRecords: response,
+        fullColorList: [],
+        colorsCount: [],
+        signedup: [],
+        viewedProfile: [],
+        viewedItem: [],
+        purchasedItem: []
+      }
     };
 
     $scope.users.forEach(function(user) {
@@ -33,22 +39,28 @@ siteApp.controller('StatsCtrl', function($scope, statsFactory) {
       genderCheck(user);
 
       // get a list of all the colors
-      trueParentChild('purchased_item', 'color', user, count.fullColorList);
+      trueParentChild('purchased_item', 'color', user, stats.counts.fullColorList);
 
       // sign up stats
-      trueParentChild('signed_up', 'date', user, count.signedup);
+      trueParentChild('signed_up', 'date', user, stats.counts.signedup);
 
       // viewed a profile
-      trueParentChild('viewed_profile', 'date', user, count.viewedProfile);
+      trueParentChild('viewed_profile', 'date', user, stats.counts.viewedProfile);
 
       // viewed an item
-      trueParentChild('viewed_item', 'date', user, count.viewedItem);
+      trueParentChild('viewed_item', 'date', user, stats.counts.viewedItem);
 
       // purchased item
-      trueParentChild('purchased_item', 'date', user, count.purchasedItem);
+      trueParentChild('purchased_item', 'date', user, stats.counts.purchasedItem);
 
       // conversions
-      testConversions('signed_up', 'purchased_item', user, '', count.conversions);
+      conversions('signed_up', 'purchased_item', user, '', stats.percents.conversions);
+
+      // conversions with viewed profile conditional
+      conversions('signed_up', 'purchased_item', user, 'viewed_profile', stats.percents.conversionsViewedProfile);
+
+      // conversions with viewed profile conditional
+      conversions('signed_up', 'purchased_item', user, 'viewed_item', stats.percents.conversionsViewedItem);
     });
 
     // checks if user has parent prop and child prop, then adds value to array
@@ -62,68 +74,63 @@ siteApp.controller('StatsCtrl', function($scope, statsFactory) {
     function genderCheck(user) {
       switch (user.gender) {
         case 'Male':
-          count.males.push('m');
+          stats.percents.males.push(user);
           break;
         case 'Female':
-          count.females.push('f');
+          stats.percents.females.push(user);
           break;
       }
     }
 
-    // makes percent
-    function getPercent(item) {
-      return (item / $scope.totalRecords) * 100;
+    // returns percent of total records
+    function getPercent(item, totalItems) {
+      return (item / totalItems) * 100;
     }
 
-    $scope.gender = {
-      men: getPercent(count.males.length),
-      women: getPercent(count.females.length)
-    };
-
     // get unique color
-    var uniqueColorList = count.fullColorList.filter(function (keyword, index) {
-      return count.fullColorList.lastIndexOf(keyword) === index;
+    var uniqueColorList = stats.counts.fullColorList.filter(function (keyword, index) {
+      return stats.counts.fullColorList.lastIndexOf(keyword) === index;
     });
 
-    // add colors length to scope
-    $scope.colorsCount = uniqueColorList.length;
+    // add colors length to stats
+    stats.counts.colorsCount = uniqueColorList;
 
+    // gets conversions
+    function conversions(signedup, purchased, user, conditional, arrayCollection) {
 
+      //console.log(conditional);
 
-    // tests conversions
-    function testConversions(signedup, purchased, user, viewed, arrayCollection) {
       // check if user has signed up and has purchased prop
       if (user.hasOwnProperty(signedup) && user.hasOwnProperty(purchased)) {
         // check if purchased was comeplete, by checking date
         if (user[purchased].hasOwnProperty('date')) {
           // if the user signup date is the same or before purchase date
           if (new Date(user[signedup].date) <= new Date(user[purchased].date)) {
-            console.log(user);
-            console.log(user[signedup].date);
-            console.log(user[purchased].date);
-            console.log('---------------');
             arrayCollection.push(user);
           }
         }
       }
     }
 
-
-
-    // adds count props to scope
+    // adds stats props to scope
     function buildScopeVars(props) {
-      for (var prop in props) {
-          if (props.hasOwnProperty(prop)) {
-              $scope[prop] = props[prop].length;
-          }
+      // gets length for counted items
+      if (props.counts) {
+        for (var count in props.counts) {
+          $scope[count] = props.counts[count].length;
+        }
+      }
+
+      // gets percent of total items
+      if (props.percents) {
+        for (var percent in props.percents) {
+          $scope[percent] = getPercent(props.percents[percent].length, $scope.totalRecords);
+        }
       }
     }
 
-    buildScopeVars(count);
-
-
-
-    console.log(count);
+    // call to build buil scope vars
+    buildScopeVars(stats);
 
     $scope.addRow = function() {
       statsFactory.getUser()
